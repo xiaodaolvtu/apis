e269d34a7eedb5a6cfc5936ab0944fa807f974ed
qiang.li <qiang.li@audaque.com>
2017-06-16 18:01:33 +0800

    添加model和server方法

--- app.js
+++ app.js
@@ -5,19 +5,42 @@ var restify = require('restify');
 
 
 const server = restify.createServer({
-    name: 'ApiServer',
+    name: 'apiserver',
     version: '1.0.0'
 });
 server.use(restify.plugins.acceptParser(server.acceptable));
 server.use(restify.plugins.queryParser());
 server.use(restify.plugins.bodyParser());
 
+const hello = 'hello'
+const Token = 'aaab'
+function checkAuth(token){
+    return token == 'aaab'
+}
 
-server.get('/echo/:name', function (req, res, next) {
-    res.send(req.params);
+function send(req, res, next, auth) {
+
+    console.log(auth+'' + req.params.access_token + checkAuth(req.params.access_token))
+    if(auth && req.params.access_token && checkAuth(req.params.access_token)) {
+        console.log("认证成功")
+        res.send('hello ' + req.params.id);
+    }
+    else  res.send(203);
+    return next();
+}
+let handleIt = (req, res, next)=>send(req, res, next, true)
+
+server.post('/'+hello, function create(req, res, next) {
+    res.send(201, Math.random().toString(36).substr(3, 8));
     return next();
 });
-
+server.get('/'+hello+'/:id', handleIt);
+// server.put('/hello', send);
+// server.head('/hello/:name', send);
+// server.del('hello/:name', function rm(req, res, next) {
+//     res.send(204);
+//     return next();
+// });
 server.listen(8080, function () {
     console.log('%s listening at %s', server.name, server.url);
 });
--- model.js
+++ model.js
@@ -1,4 +1,60 @@
 /**
  * Created by qiangli on 2017/6/16.
  */
-var test = {}
+// get<get>:
+// /test
+// /test/{id}     <filter>
+// /test/{id}/exists   <filter>
+// set<post>:
+// /test  data:{item}
+
+////不是所有浏览器都支持delete,put请求，所以用post代替
+// delete<post>:
+// /test/{id}/delete
+function checkAuth(token){
+    console.log("checkAuth")
+    return token == 'aaab'
+}
+
+function send(req, res, next, auth) {
+    if(auth && req.params.access_token && checkAuth(req.params.access_token)) {
+        console.log("认证成功")
+        res.send('hello ' + req.params.id);
+    }
+    else  res.send(203);
+    return next();
+}
+
+createApis(modelName, permission){
+    let token = '';
+    if(permission) accesstoken = getToken();
+
+}
+
+
+createModel(server, modelName, auth=true, model){
+    const root = /'+modelName;
+    let handleIt = (req, res, next)=>send(req, res, next, auth)
+
+    //create api (modelName as the table)
+    server.get(root, handleIt);  //select * from table
+
+    server.get(root+'/:id', handleIt);//select id from table
+
+    server.post(root, function create(req, res, next) {//select * from table where filter or do something else
+        const params = res.params;
+        res.send(201, Math.random().toString(36).substr(3, 8));
+        return next();
+    });
+
+    server.post(root+'/:id',function create(req, res, next) { //select id from table where filter
+        const params = res.params;
+        const filter = params.filter;
+        res.send(201, Math.random().toString(36).substr(3, 8));
+        return next();
+    });
+    //create table
+
+    //create filter
+
+}
