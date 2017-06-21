0dccd4a9820d09e69d89c2a5879e3d470ef68ee6
xdaoo <399198837@qq.com>
2017-06-21 14:36:55 +0800

    Add restify oauth2

--- app.js
+++ app.js
@@ -4,6 +4,8 @@
 // var Router = require('restify-router').Router;  //静态文件路由支持
 // var routerInstance = new  Router();
 var restify = require('restify');
+var restifyOAuth2 = require("restify-oauth2");
+var hooks = require("./hooks");
 var sql = require("./sql.js"); // 数据库连接池
 var { createRouters } = require("./model.js");
 function customRouters(server,auth){
@@ -16,9 +18,14 @@ const server = restify.createServer({
     name: 'apiserver',
     version: '1.0.0'
 });
+server.use(restify.authorizationParser());
 server.use(restify.plugins.acceptParser(server.acceptable));
 server.use(restify.plugins.queryParser());
-server.use(restify.plugins.bodyParser());
+//server.use(restify.plugins.bodyParser());
+server.use(restify.plugins.bodyParser({ mapParams: false  }));
+//restifyOAuth2.cc(server, options);
+// or
+restifyOAuth2.ropc(server, { tokenEndpoint: '/token', hooks: hooks , tokenExpirationTime: 3600});
 
 //从数据库中，取得filter json 和 model。
 sql.connect(function(err, client, done) {
@@ -31,7 +38,53 @@ sql.connect(function(err, client, done) {
 //    customRouters(server)
 });
 //
+server.get('/', function (req, res) {
+    var response = {
+        _links: {
+            self: { href: RESOURCES.INITIAL },
+            "http://www.baidu.com": { href:'' }
+        }
+    };
 
-server.listen(8080, function () {
+    if (req.username) {
+        response._links["http://rel.example.com/secret"] = { href: RESOURCES.SECRET };
+    } else {
+        response._links["oauth2-token"] = {
+            href: RESOURCES.TOKEN,
+            "grant-types": "password",
+            "token-types": "bearer"
+        };
+    }
+
+    res.contentType = "application/hal+json";
+    res.send(response);
+});
+
+server.get('/login', function(req, res){
+    const parmas = req.params||{};
+    const username = parmas.username
+    console.log(JSON.stringify(username))
+    if(!username) res.send(403,{a:2});
+    else sql.connect(function(err, client, done) {
+        if(err) {
+
+            console.log("@2:")
+            return console.error('数据库连接出错', err);
+        }
+        //连接成功，从models表中取得model，然后生成路由
+        client.query('SELECT token from user where username = '+ username,[], function(err, result) {
+            done();// 释放连接（将其返回给连接池）
+            if(err) {
+                return console.error('查询出错', err);
+            }
+            //command, rowCount, oid , rows
+            console.log("@1:"+result.rows[0])
+            res.send(200,{a:1});
+        });
+        //自定义路由
+//    customRouters(server)
+    });
+});
+server.listen(9999, function () {
     console.log('%s listening at %s', server.name, server.url);
 });
---
+++ hooks.js
@@ -0,0 +1,68 @@
+/**
+ * Created by qiangli on 2017/6/21.
+ */
+"use strict";
+
+var _ = require("underscore");
+var crypto = require("crypto");
+
+var database = {
+    clients: {
+        officialApiClient: { secret: "C0FFEE" },
+        unofficialClient: { secret: "DECAF" }
+    },
+    users: {
+        AzureDiamond: { password: "hunter2" },
+        Cthon98: { password: "*********" }
+    },
+    tokensToUsernames: {}
+};
+
+function generateToken(data) {
+    var random = Math.floor(Math.random() * 100001);
+    var timestamp = (new Date()).getTime();
+    var sha256 = crypto.createHmac("sha256", random + "WOO" + timestamp);
+
+    return sha256.update(data).digest("base64");
+}
+
+exports.validateClient = function (credentials, req, cb) {
+    // Call back with `true` to signal that the client is valid, and `false` otherwise.
+    // Call back with an error if you encounter an internal server error situation while trying to validate.
+
+    var isValid = _.has(database.clients, credentials.clientId) &&
+        database.clients[credentials.clientId].secret === credentials.clientSecret;
+    cb(null, isValid);
+};
+
+exports.grantUserToken = function (credentials, req, cb) {
+    var isValid = _.has(database.users, credentials.username) &&
+        database.users[credentials.username].password === credentials.password;
+    if (isValid) {
+        // If the user authenticates, generate a token for them and store it so `exports.authenticateToken` below
+        // can look it up later.
+
+        var token = generateToken(credentials.username + ":" + credentials.password);
+        database.tokensToUsernames[token] = credentials.username;
+
+        // Call back with the token so Restify-OAuth2 can pass it on to the client.
+        return cb(null, token);
+    }
+
+    // Call back with `false` to signal the username/password combination did not authenticate.
+    // Calling back with an error would be reserved for internal server error situations.
+    cb(null, false);
+};
+
+exports.authenticateToken = function (token, req, cb) {
+    if (_.has(database.tokensToUsernames, token)) {
+        // If the token authenticates, set the corresponding property on the request, and call back with `true`.
+        // The routes can now use these properties to check if the request is authorized and authenticated.
+        req.username = database.tokensToUsernames[token];
+        return cb(null, true);
+    }
+
+    // If the token does not authenticate, call back with `false` to signal that.
+    // Calling back with an error would be reserved for internal server error situations.
+    cb(null, false);
+};
